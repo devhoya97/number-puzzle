2f355e3aefd4e9c18747863403cbc9ccb885b6dd
devhoya97 <diajbh123@gmail.com>
2023-11-16 19:12:08 +0900

    feat: 정답을 맞출 때까지 게임을 수행하는 기능 구현

--- src/main/java/puzzle/Puzzle.java
+++ src/main/java/puzzle/Puzzle.java
@@ -1,6 +1,7 @@
 package puzzle;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -8,6 +9,18 @@ public class Puzzle {
     private static final int PUZZLE_LENGTH = 8;
     public static void main(String[] args) {
         System.out.println("간단 숫자 퍼즐");
+    }
+
+    public static void playTurns() {
+        List<Integer> randomNumbers = createRandomNumbers();
+        List<Integer> answer = new ArrayList<>(randomNumbers);
+        Collections.sort(answer);
+        int turn = 0;
+        while(!randomNumbers.equals(answer)) {
+            turn++;
+            playTurn(turn);
+        }
+        System.out.println("축하합니다! " + turn + "턴만에 퍼즐을 완성하셨습니다!");
     }
 
     public static List<Integer> createRandomNumbers() {
@@ -22,4 +35,9 @@ public class Puzzle {
         }
         return randomNumbers;
     }
+
+    public static void playTurn(int turn) {
+
+    }
 }
+
