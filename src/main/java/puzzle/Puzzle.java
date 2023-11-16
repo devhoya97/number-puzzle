3497ff4b15fd98d6502dcca31cea8fc28429b685
devhoya97 <diajbh123@gmail.com>
2023-11-16 19:36:17 +0900

    feat: 한 턴을 수행하는 기능의 뼈대 구현

--- src/main/java/puzzle/Puzzle.java
+++ src/main/java/puzzle/Puzzle.java
@@ -4,21 +4,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Puzzle {
     private static final int PUZZLE_LENGTH = 8;
+    private static final int CHANGE_TARGETS_SIZE = 2;
+    private static final String DELIMITER = ",";
+    private static final String INPUT_ERROR = "잘못 입력하셨습니다. 다시 입력해 주세요.";
+
     public static void main(String[] args) {
-        System.out.println("간단 숫자 퍼즐");
+
     }
 
     public static void playTurns() {
+        System.out.println("간단 숫자 퍼즐");
         List<Integer> randomNumbers = createRandomNumbers();
         List<Integer> answer = new ArrayList<>(randomNumbers);
         Collections.sort(answer);
         int turn = 0;
         while(!randomNumbers.equals(answer)) {
             turn++;
-            playTurn(turn);
+            playTurn(randomNumbers, turn);
         }
         System.out.println("축하합니다! " + turn + "턴만에 퍼즐을 완성하셨습니다!");
     }
@@ -36,8 +42,18 @@ public class Puzzle {
         return randomNumbers;
     }
 
-    public static void playTurn(int turn) {
+    public static void playTurn(List<Integer> randomNumbers, int turn) {
+        System.out.println("Trun " + turn);
+        System.out.println(randomNumbers);
+        try {
+            List<Integer> changeTargets = getChangeTargets();
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+        }
+    }
 
+    public static List<Integer> getChangeTargets() {
+        return null;
     }
 }
 
