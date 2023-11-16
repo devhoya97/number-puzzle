3eadb19bde14d1292f8849ef53d765d98a3f9a5a
devhoya97 <diajbh123@gmail.com>
2023-11-16 18:59:38 +0900

    feat: 랜덤 숫자 리스트 생성기능 구현

--- src/main/java/puzzle/Puzzle.java
+++ src/main/java/puzzle/Puzzle.java
@@ -1,4 +1,25 @@
 package puzzle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Puzzle {
+    private static final int PUZZLE_LENGTH = 8;
+    public static void main(String[] args) {
+        System.out.println("간단 숫자 퍼즐");
+    }
+
+    public static List<Integer> createRandomNumbers() {
+        Random random = new Random();
+        List<Integer> randomNumbers = new ArrayList<>(PUZZLE_LENGTH);
+        while(randomNumbers.size() < PUZZLE_LENGTH) {
+            int randomNumber = random.nextInt(PUZZLE_LENGTH) + 1;
+            if (randomNumbers.contains(randomNumber)) {
+                continue;
+            }
+            randomNumbers.add(randomNumber);
+        }
+        return randomNumbers;
+    }
 }
