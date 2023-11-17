c7b0e4ab5213423880eb77cafcd5d7f67b157491
devhoya97 <diajbh123@gmail.com>
2023-11-17 18:05:01 +0900

    feat: 현재 공백의 위치를 알아내는 기능 구현

--- src/main/java/puzzle/Puzzle.java
+++ src/main/java/puzzle/Puzzle.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 public class Puzzle {
     private static int ROW_LENGTH = 4;
     private static int COLUMN_LENGTH = 4;
+    private static int EMPTY_EXPRESSION = 16;
+    private static int ERROR_RETURN = -1;
 
     public static void main(String[] args) {
 
@@ -24,6 +26,24 @@ public class Puzzle {
             answer.add(row);
         }
         return answer;
+    }
+
+    private static void shuffleGameBoard(List<List<Integer>> gameBoard) {
+
+    }
+
+    private static Integer getBlankRow(List<List<Integer>> gameBoard) {
+        for (int rowCount = 0; rowCount < ROW_LENGTH; rowCount++) {
+            List<Integer> row = gameBoard.get(rowCount);
+            if (row.contains(EMPTY_EXPRESSION)) {
+                return rowCount;
+            }
+        }
+        throw new IllegalStateException("게임보드에서 공백을 찾을 수 없습니다.");
+    }
+
+    private static Integer getBlankColumn(List<Integer> row) {
+        return row.indexOf(EMPTY_EXPRESSION);
     }
 
     private static void printGameBoard(List<List<Integer>> gameBoard) {
